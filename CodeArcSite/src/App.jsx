44d2520c661a15f0b17a14c8e854e6ead91e2a2b
Abdulhakim Fedlu <155722598+abdulhakimfedlu@users.noreply.github.com>
2025-06-01 15:39:58 +0300

    Update App.jsx

--- CodeArcSite/src/App.jsx
+++ CodeArcSite/src/App.jsx
@@ -14,6 +14,7 @@ function App() {
     return savedMode ? JSON.parse(savedMode) : window.matchMedia('(prefers-color-scheme: dark)').matches;
   });
 
+  
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   useEffect(() => {

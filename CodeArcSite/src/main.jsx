7e59ce65dd3b207ed7da2f69fe10e8f597a5e791
Abdulhakim Fedlu <155722598+abdulhakimfedlu@users.noreply.github.com>
2025-06-01 15:42:44 +0300

    Update main.jsx

--- CodeArcSite/src/main.jsx
+++ CodeArcSite/src/main.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import './index.css'; // Import Tailwind and custom styles
+import './index.css'; 
+// Import Tailwind and custom styless
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>

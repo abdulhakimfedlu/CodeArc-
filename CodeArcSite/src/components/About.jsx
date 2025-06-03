5803239682b3d894667be6b49e7f69824a2d09c1
Abdulhakim Fedlu <155722598+abdulhakimfedlu@users.noreply.github.com>
2025-06-03 22:49:44 +0300

    Update About.jsx

--- CodeArcSite/src/components/About.jsx
+++ CodeArcSite/src/components/About.jsx
@@ -8,6 +8,7 @@ const About = () => {
           {/* Left Side: Why Join */}
           <div className="space-y-8">
             <div className="relative">
+              
               <h2 className="text-5xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-cyan-400 to-purple-600 reveal">
                 Why Join?
               </h2>

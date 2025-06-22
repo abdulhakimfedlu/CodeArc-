d3226bbcd42021aa340bb1033f810be93481f263
Abdulhakim Fedlu <155722598+abdulhakimfedlu@users.noreply.github.com>
2025-06-22 22:03:45 +0300

    Update App.jsx

--- CodeArcSite/src/App.jsx
+++ CodeArcSite/src/App.jsx
@@ -8,7 +8,6 @@ import Contact from './components/Contact';
 import Footer from './components/Footer';
 
 
-
 function App() {
   const [isDarkMode, setDarkMode] = useState(() => {
     const savedMode = localStorage.getItem('darkMode');

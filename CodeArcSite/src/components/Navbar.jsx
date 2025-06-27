3015d7a23797f5b8a04e8d7ef4bae83101599144
Abdulhakim Fedlu <155722598+abdulhakimfedlu@users.noreply.github.com>
2025-06-27 22:02:59 +0300

    Update Navbar.jsx

--- CodeArcSite/src/components/Navbar.jsx
+++ CodeArcSite/src/components/Navbar.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 
-
 const Navbar = ({ isDarkMode, setDarkMode, isMenuOpen, setIsMenuOpen }) => {
   return (
     <nav className="fixed top-0 left-0 w-full z-50 bg-black/70 backdrop-blur-md border-b border-gray-800">
